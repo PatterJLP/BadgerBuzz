dc218af89161c6e1d1ade1e3e12b9bb7c8b0af79
Jack Patterson <patterjlp@gmail.com>
2024-12-03 12:03:57 -0600

    Added user log out with menu icon and made it so users can't backtrack to map

--- app/src/main/java/com/cs407/badgerbuzz/MapsFragment.kt
+++ app/src/main/java/com/cs407/badgerbuzz/MapsFragment.kt
@@ -1,15 +1,15 @@
 package com.cs407.badgerbuzz
 
 import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.Spinner
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,14 +17,28 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
 
 class MapsFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
 
     private val callback = OnMapReadyCallback { googleMap ->
 
         val Madison = LatLng(43.07340550591327, -89.40070146109815)
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Madison, 15f))
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +53,28 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+
+        val menuIcon: ImageView = view.findViewById(R.id.menu_icon)
+        menuIcon.setOnClickListener { v ->
+            val popupMenu = PopupMenu(requireContext(), v)
+            popupMenu.menuInflater.inflate(R.menu.menu_main, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.action_logout -> {
+                        Firebase.auth.signOut()
+                        clearBackStack()
+                        navigateToFragment(LoginFragment::class.java,null)
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popupMenu.show()
+        }
+
+
+
 
 
 
@@ -60,7 +96,7 @@ class MapsFragment : Fragment() {
 
     }
 
-    private fun navigateToFragment(fragmentClass: Class<out Fragment>, backStackName: String) {
+    private fun navigateToFragment(fragmentClass: Class<out Fragment>, backStackName: String?) {
 
         parentFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainerView, fragmentClass, null)
@@ -68,4 +104,14 @@ class MapsFragment : Fragment() {
             .addToBackStack(backStackName)
             .commitAllowingStateLoss()
     }
+
+    private fun clearBackStack() {
+        val fragmentManager = parentFragmentManager
+        if (fragmentManager.backStackEntryCount > 0) {
+            for (i in 0 until fragmentManager.backStackEntryCount) {
+                fragmentManager.popBackStack()
+            }
+        }
+    }
+
 }
