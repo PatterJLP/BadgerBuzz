f37fe549d618cf5687cc4092f44553226f03280c
rachitbandaram <rachitbandaram@gmail.com>
2024-12-05 15:34:26 -0600

    Added the event list functionality

--- app/src/main/java/com/cs407/badgerbuzz/EventListFragment.kt
+++ app/src/main/java/com/cs407/badgerbuzz/EventListFragment.kt
@@ -1,28 +1,53 @@
 package com.cs407.badgerbuzz
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.gms.maps.GoogleMap
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private lateinit var auth: FirebaseAuth
+private lateinit var googleMap: GoogleMap
+
+
 
 /**
  * A simple [Fragment] subclass.
  * Use the [EventListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+data class Event(
+    val title: String,
+    val date: String,
+    val location: String
+)
+
+
 class EventListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private val db = FirebaseFirestore.getInstance()
+    private val list = mutableListOf<Event>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        retrieveFromDB()
+        auth = Firebase.auth
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -32,6 +57,7 @@ class EventListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.event_list_fragment, container, false)
@@ -56,4 +82,41 @@ class EventListFragment : Fragment() {
                 }
             }
     }
+    private fun retrieveFromDB(){
+        db.collection("events")
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                for (document in querySnapshot) {
+                    val eventId = document.id
+                    val eventName = document.getString("eventName")
+                    val location = document.getGeoPoint("location")
+
+                    val eventDetails = Event(eventId, eventName.toString(), location.toString())
+                    list.add(eventDetails)
+                    addEvent(eventDetails)
+
+
+                }
+
+
+            }
+            .addOnFailureListener { exception ->
+                exception.printStackTrace()
+            }
+    }
+
+    private fun addEvent(eventDetails: Event){
+        val eventContainer: LinearLayout = requireView().findViewById(R.id.eventContainer)
+        val eventView = TextView(requireContext())
+        eventView.text = "Event: ${eventDetails.title}\nDate: (${eventDetails.date},\n Location: ${eventDetails.location})"
+        eventView.textSize = 16f
+        eventView.setPadding(16, 16, 16, 16)
+
+        // Add the TextView to the LinearLayout
+        eventContainer.addView(eventView)
+
+
+
+    }
+
 }
