8b0c2e4d47c6a8696dfc41a67424f5a3a8b84cf1
Kobey Frey <kfrey6@wisc.edu>
2024-12-08 19:47:59 -0600

    started image uploading

--- app/src/main/java/com/cs407/badgerbuzz/PostEventFragment.kt
+++ app/src/main/java/com/cs407/badgerbuzz/PostEventFragment.kt
@@ -3,6 +3,7 @@ package com.cs407.badgerbuzz
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.app.TimePickerDialog
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,18 +17,25 @@ import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.TakePicturePreview
 import com.cs407.badgerbuzz.DatePickerFragment.OnDateSelectedListener
+import com.google.firebase.Firebase
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storage
 import java.util.Calendar
 import java.util.Date
 import kotlin.math.log
 import kotlin.math.min
 
-class PostEventFragment : Fragment(), DatePickerFragment.OnDateSelectedListener,TimePickerFragment.OnTimeSelectedListener {
+class PostEventFragment : Fragment(), DatePickerFragment.OnDateSelectedListener,
+    TimePickerFragment.OnTimeSelectedListener {
     val db = FirebaseFirestore.getInstance()
     private var startDate: String? = null
     private var endDate: String? = null
@@ -40,9 +48,17 @@ class PostEventFragment : Fragment(), DatePickerFragment.OnDateSelectedListener,
     private var imageURL: String? = null
 
 
-
-
-    private fun addEvent(eventName: String,description: String, imageUrl: String, latitude: Double, longitude: Double, startTime: String, endTime: String, startDate: String, endDate: String){
+    private fun addEvent(
+        eventName: String,
+        description: String,
+        imageUrl: String,
+        latitude: Double,
+        longitude: Double,
+        startTime: String,
+        endTime: String,
+        startDate: String,
+        endDate: String
+    ) {
         val event = hashMapOf(
             "eventName" to eventName,
             "description" to description,
@@ -97,10 +113,13 @@ class PostEventFragment : Fragment(), DatePickerFragment.OnDateSelectedListener,
             timePickerFragment.setOnTimeSelectedListener(this)
             timePickerFragment.show(parentFragmentManager, "timePicker")
         }
-        requireView().findViewById<ImageButton>(R.id.currentLocationButton).setOnClickListener{
-            navigateToFragment(LocationFragment::class.java,"showing location")
+        requireView().findViewById<ImageButton>(R.id.currentLocationButton).setOnClickListener {
+            navigateToFragment(LocationFragment::class.java, "showing location")
         }
-        parentFragmentManager.setFragmentResultListener("locationKey",viewLifecycleOwner){_, bundle ->
+        parentFragmentManager.setFragmentResultListener(
+            "locationKey",
+            viewLifecycleOwner
+        ) { _, bundle ->
             latitude = bundle.getDouble("latitude")
             longitude = bundle.getDouble("longitude")
             val latLng = requireView().findViewById<TextView>(R.id.EventLocationEditText)
@@ -112,20 +131,55 @@ class PostEventFragment : Fragment(), DatePickerFragment.OnDateSelectedListener,
 
         requireView().findViewById<Button>(R.id.postEventButton).setOnClickListener {
             eventName = requireView().findViewById<EditText>(R.id.eventNameEditText).text.toString()
-            description = requireView().findViewById<EditText>(R.id.eventDescriptionEditText).text.toString()
-            if(eventName == null || description == null || latitude == null || longitude == null || startTime == null || endTime == null || startDate == null || endDate == null){
+            description =
+                requireView().findViewById<EditText>(R.id.eventDescriptionEditText).text.toString()
+            if (eventName == null || description == null || latitude == null || longitude == null || startTime == null || endTime == null || startDate == null || endDate == null) {
                 Toast.makeText(
                     requireContext(),
                     "Please Make Sure All Fields Are Filled",
                     Toast.LENGTH_SHORT,
                 ).show()
-            }else {
-                if (imageURL == null){
-                    imageURL = "https://brand.wisc.edu/content/uploads/2023/09/vert-w-crest-logo-web-digital-color.png"
+            } else {
+                if (imageURL == null) {
+                    imageURL =
+                        "https://brand.wisc.edu/content/uploads/2023/09/vert-w-crest-logo-web-digital-color.png"
                 }
-                addEvent(eventName!!,description!!,imageURL!!,latitude!!,longitude!!,startTime!!,endTime!!,startDate!!,endDate!!)
+                addEvent(
+                    eventName!!,
+                    description!!,
+                    imageURL!!,
+                    latitude!!,
+                    longitude!!,
+                    startTime!!,
+                    endTime!!,
+                    startDate!!,
+                    endDate!!
+                )
             }
             navigateToFragment(MapsFragment::class.java, "showing Map")
+        }
+
+        val storage = Firebase.storage
+        // Create a storage reference from our app
+        val storageRef = storage.reference
+
+        // Create a reference to "mountains.jpg"
+        val imageRef = storageRef.child("mountains.jpg")
+
+        // Create a reference to 'images/mountains.jpg'
+        val mountainImagesRef = storageRef.child("images/mountains.jpg")
+
+        // While the file names are the same, the references point to different files
+        //mountainsRef.name == mountainImagesRef.name // true
+        //mountainsRef.path == mountainImagesRef.path // false
+
+        val eventImage = requireView().findViewById<ImageView>(R.id.eventImage)
+        val takePicturePreview =
+            registerForActivityResult(TakePicturePreview()) { thumbnail: Bitmap? ->
+                eventImage.setImageBitmap(thumbnail)
+            }
+        requireView().findViewById<Button>(R.id.addImageButton).setOnClickListener {
+            takePicturePreview.launch(null)
         }
     }
 
@@ -142,6 +196,7 @@ class PostEventFragment : Fragment(), DatePickerFragment.OnDateSelectedListener,
             endEvent.text = endDate
         }
     }
+
     override fun onTimeSelected(time: String) {
         if (startTime == null) {
             startTime = time
@@ -164,33 +219,32 @@ class PostEventFragment : Fragment(), DatePickerFragment.OnDateSelectedListener,
 }
 
 
+class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+    private var listener: OnTimeSelectedListener? = null
 
-
-    class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
-        private var listener: OnTimeSelectedListener? = null
-
-        interface OnTimeSelectedListener {
-            fun onTimeSelected(time: String)
-        }
-        fun setOnTimeSelectedListener(listener: OnTimeSelectedListener) {
-            this.listener = listener
-        }
-
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            // Use the current time as the default values for the picker.
-            val c = Calendar.getInstance()
-            val hour = c.get(Calendar.HOUR_OF_DAY)
-            val minute = c.get(Calendar.MINUTE)
-
-            // Create a new instance of TimePickerDialog and return it.
-            return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
-        }
-
-        override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-            val selectedTime = "$hourOfDay:$minute"
-            listener?.onTimeSelected(selectedTime)
-        }
+    interface OnTimeSelectedListener {
+        fun onTimeSelected(time: String)
     }
+
+    fun setOnTimeSelectedListener(listener: OnTimeSelectedListener) {
+        this.listener = listener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current time as the default values for the picker.
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        // Create a new instance of TimePickerDialog and return it.
+        return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+        val selectedTime = "$hourOfDay:$minute"
+        listener?.onTimeSelected(selectedTime)
+    }
+}
 
 
 class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
